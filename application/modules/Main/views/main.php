e5b5a3c01ac634caf6d1e87163a7bc750c273f51
aveo-git <aveocod3@gmail.com>
2022-03-03 11:03:19 +0300

    navbar

--- application/modules/Main/views/main.php
+++ application/modules/Main/views/main.php
@@ -1,18 +1,3 @@
-<div class="row justify-content-center w-75 m-auto">
-   <div class="col-lg-8">
-      <div class="segment">
-         A1
-      </div>
-      <div class="segment">
-         A2
-      </div>
-   </div>
-   <div class="col-lg-4">
-      <div class="segment">
-         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facere libero fugit ad sapiente aliquid hic numquam vel molestiae</h1>
-      </div>
-      <div class="segment">
-         <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et tempora repellendus qui</h2>
-      </div>
-   </div>
+<div class="segment">
+   <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facere libero fugit ad sapiente aliquid hic numquam vel molestiae</h1>
 </div>
